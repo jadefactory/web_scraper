b575c18a7c071f11814d8dd4e35c6c64f152795e
jadefactory <seychelle86@gmail.com>
2022-08-19 11:40:12 +0900

    poi crawler

---
+++ naver/poi_crawler.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+url = "https://map.naver.com/v5/"
+browser = webdriver.Chrome("/Users/jade/Desktop/portfolio/_web_crawling/driver/chromedriver")
+browser.implicitly_wait(10)
+browser.maximize_window()
+browser.get(url)
+
+# 검색창 입력
+search = browser.find_element_by_css_selector("input.input_search")
+search.click()
+time.sleep(1)
+search.send_keys("강남역맛집")
+time.sleep(1)
+search.send_keys(Keys.ENTER)
+time.sleep(2)
+
+# 가게 이름 10개 가져오기
+names = browser.find_elements_by_css_selector("span.OXiLu")
+print(names)
