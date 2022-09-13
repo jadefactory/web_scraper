83734a445926d3cf15ae937f3370d633dacc436e
jadefactory <seychelle86@gmail.com>
2022-09-13 23:27:58 +0900

    toyoko-inn slack bot update

--- toyoko_inn/remain_room_finder.py
+++ toyoko_inn/remain_room_finder.py
@@ -1,5 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
+token = "Slack API KEY"
+channel = "Slack Channel"
+
+def post_message(token, channel, text):
+    requests.post("https://slack.com/api/chat.postMessage",
+        headers = {"Authorization":"Bearer" + token},
+        data = {"channel":channel, "text": text}
+    )
 
 url = "https://www.toyoko-inn.com/korea/search"
 
@@ -16,8 +26,26 @@ data_obj = {
     'sel_ldgngPpl': '1'
 }
 
-response = requests.post(url, data=data_obj)
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-beds = soup.select("ul.btnLink03")
-print(beds)
+cnt = 1
+while True:
+    try:
+        response = requests.post(url, data=data_obj)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        beds = soup.select("ul.btnLink03")
+
+        for i, bed in enumerate(beds, 1):
+            links = bed.select('a')
+            if len(links) > 0:
+                if i <= 3:
+                    post_message(token, channel, "싱글 잔실 있어요!")
+                    
+                elif i <= 5:
+                    post_message(token, channel, "더블 잔실 있어요!")
+            
+            print(f'{cnt}번째 시도입니다.')
+            time.sleep(10)
+            cnt += 1
+    
+    except:
+        print("오류가 발생했지만 계속 실행")
